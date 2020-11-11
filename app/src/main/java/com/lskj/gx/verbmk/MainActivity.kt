eddc44b666471ed873391f4dd91a9783c8eba341
tongzhiwei <clodthunder@gmail.com>
2020-11-11 17:58:24 +0800

    初始化 kotlin 语言的 多module 项目

---
+++ app/src/main/java/com/lskj/gx/verbmk/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lskj.gx.verbmk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
